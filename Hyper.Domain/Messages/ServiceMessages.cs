9d200730ce59bf5e1f897d8ad5bcd3c67af1cb47
cesarbmx <cesarbmx@hotmail.com>
2018-07-25 12:05:30 -0400

    Refactor

--- Hyper.Domain/Messages/ServiceMessages.cs
+++ Hyper.Domain/Messages/ServiceMessages.cs
@@ -7,7 +7,6 @@ namespace Hyper.Domain.Messages
         public const string InvalidRequest = "(#0501) The request is invalid";
         public const string ValidationFailed = "(#0502) Validation failed";
         public const string ResourceNotFound = "(#0503) The resoruce was not found";
-        public const string Conflict = "(#0504) There is a conflict with the current state";
-        public const string InternalServerError = "(#0505) Internal Server Error";
+        public const string InternalServerError = "(#0504) Internal Server Error";
     }
 }
