04d1e5b586d8537fcfe1f8736ea70911e9fdd03c
cesarc <cesarc@pinnaclesports.com>
2018-10-26 12:45:32 -0400

    idem

--- Hyper.Api/Configuration/AutomapperConfig.cs
+++ Hyper.Api/Configuration/AutomapperConfig.cs
@@ -1,8 +1,11 @@
-﻿using AutoMapper;
+﻿using System;
+using AutoMapper;
+using CoinMarketCap.Entities;
 using Microsoft.Extensions.DependencyInjection;
 using Hyper.Api.Responses;
 using Hyper.Domain.Models;
 using Hyper.Shared.Extensions;
+using Version = Hyper.Domain.Models.Version;
 
 namespace Hyper.Api.Configuration
 {
@@ -14,7 +17,7 @@ namespace Hyper.Api.Configuration
 
                 cfg => {
                     
-                    // Api
+                    // Responses
                     cfg.CreateMap<Version, VersionResponse>();
                     cfg.CreateMap<Health, HealthResponse>();
                     cfg.CreateMap<Version, VersionResponse>()
@@ -24,9 +27,12 @@ namespace Hyper.Api.Configuration
                     cfg.CreateMap<Currency, CurrencyResponse>();
                     cfg.CreateMap<Log, LogResponse>();
 
-                    // Persistence
-                    cfg.AddProfile(new Persistence.Configuration.AutomapperConfig());
-                    
+                    // Models
+                    cfg.CreateMap<TickerEntity, Currency>()
+                        .ForMember(dest => dest.Price, opt => opt.MapFrom(src => Convert.ToDecimal(src.PriceUsd)))
+                        .ForMember(dest => dest.Volume24H, opt => opt.MapFrom(src => Convert.ToDecimal(src.Volume24hUsd)))
+                        .ForMember(dest => dest.MarketCap, opt => opt.MapFrom(src => Convert.ToDecimal(src.MarketCapUsd)))
+                        .ForMember(dest => dest.PercentageChange24H, opt => opt.MapFrom(src => Convert.ToDecimal(src.PercentChange24h)));
                 });
 
             return services;
