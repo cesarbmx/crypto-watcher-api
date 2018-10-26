0d98227672250be93855959e91faae7ba545d638
cesarc <cesarc@pinnaclesports.com>
2018-10-26 09:25:29 -0400

    Use in memory database everywhere

--- Hyper.ConsoleApp/Program.cs
+++ Hyper.ConsoleApp/Program.cs
@@ -15,6 +15,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Hangfire.MemoryStorage;
+using Hyper.Domain.Repositories;
+using Hyper.Infrastructure.Repositories;
 
 namespace Hyper.ConsoleApp
 {
@@ -38,9 +41,13 @@ namespace Hyper.ConsoleApp
             var serviceProvider = new ServiceCollection()
                 .AddLogging()
                 .AddSingleton<IMapper>(factory => new Mapper(automapperConfig))
-                .AddDbContext<MainDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("Hyper")))
+                //.AddDbContext<MainDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("Hyper")))
+                .AddDbContext<MainDbContext>(options => options.UseInMemoryDatabase("Hyper"))
                 .AddSingleton<CacheService, CacheService>()
+                .AddSingleton<ICacheRepository, CacheRepository>()
                 .AddSingleton<CurrencyService, CurrencyService>()
+                .AddSingleton<LogService, LogService>()
+                .AddSingleton<ILogRepository, LogRepository>()
                 .AddSingleton<ICoinMarketCapClient, CoinMarketCapClient>()
                 .AddSingleton<CurrencyJob, CurrencyJob>()
                 .BuildServiceProvider();
@@ -57,9 +64,10 @@ namespace Hyper.ConsoleApp
             var logger = serviceProvider.GetService<ILoggerFactory>()
                 .CreateLogger<Program>();
             logger.LogInformation("LogInformation");
-            
+
             // Hangfire
-            GlobalConfiguration.Configuration.UseSqlServerStorage(configuration.GetConnectionString("Hyper"));
+            //GlobalConfiguration.Configuration.UseSqlServerStorage(configuration.GetConnectionString("Hyper"));
+            GlobalConfiguration.Configuration.UseMemoryStorage();
             GlobalConfiguration.Configuration.UseActivator(new HangfireActivator(serviceProvider));
 
             // Register jobs
