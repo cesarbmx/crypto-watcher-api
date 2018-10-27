cbc9b0a551a88093ed2e7ffcdc87077d680a17ed
cesarc <cesarc@pinnaclesports.com>
2018-10-27 07:56:43 -0400

    Audit repository

---
+++ Hyper.Infrastructure/AuditRepositories/AuditRepository.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hyper.Domain.Models;
+using Hyper.Domain.Expressions;
+using Hyper.Domain.Repositories;
+using System.Linq;
+
+namespace Hyper.Persistence.AuditRepositories
+{
+    public class AuditRepository<T>: ICacheRepository where T: new()
+    {
+        private readonly List<T> _t;
+        private readonly ILogRepository _logRepository;
+
+        public AuditRepository(ILogRepository logRepository)
+        {
+            _t = new List<T>();
+            _logRepository = logRepository;
+
+            LoadAudit();
+        }
+
+        private void LoadAudit()
+        {
+            var log = _logRepository.GetLog().Result;
+            foreach (var logEntry in log)
+            {
+                T originalValue;
+                T newValue;
+                switch (logEntry.ActionName)
+                {
+                    case "Add":
+                        newValue = logEntry.ModelJsonToObject<T>();
+                        _t.Add(newValue);
+                        break;
+                    case "Update":
+                        newValue = logEntry.ModelJsonToObject<T>();
+                        originalValue = GetByKey(originalValue.Key).Result;
+                        originalValue = newValue;
+                        break;
+                    case "Delete":
+                        newValue = logEntry.ModelJsonToObject<T>();
+                        originalValue = GetByKey(originalValue.Key).Result;
+                        _t.Remove(originalValue);
+                        break;
+                }
+            }
+        }
+
+        public  Task<Cache> GetByKey(string key)
+        {
+            // Get cache
+            return Task.FromResult(_cacheList.FirstOrDefault(CacheExpressions.HasKey(key).Compile()));
+        }
+        public void Add(Cache cache)
+        {
+            // Add
+            _cacheList.Add(cache);
+        }
+    }
+}
---
+++ Hyper.Infrastructure/AuditRepositories/CacheAuditRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hyper.Domain.Models;
+using Hyper.Domain.Expressions;
+using Hyper.Domain.Repositories;
+using System.Linq;
+
+namespace Hyper.Persistence.AuditRepositories
+{
+    public class CacheAuditRepository: ICacheRepository
+    {
+        private readonly List<Cache> _cacheList;
+        private readonly ILogRepository _logRepository;
+
+        public CacheAuditRepository(ILogRepository logRepository)
+        {
+            _cacheList = new List<Cache>();
+            _logRepository = logRepository;
+
+            LoadAudit();
+        }
+
+        private void LoadAudit()
+        {
+            var log = _logRepository.GetLog().Result;
+            foreach (var logEntry in log)
+            {
+                Cache originalValue;
+                Cache newValue;
+                switch (logEntry.ActionName)
+                {
+                    case "Add":
+                        newValue = logEntry.ModelJsonToObject<Cache>();
+                        _cacheList.Add(newValue);
+                        break;
+                    case "Update":
+                        newValue = logEntry.ModelJsonToObject<Cache>();
+                        originalValue = GetByKey(newValue.Key).Result;
+                        var index = _cacheList.IndexOf(originalValue);
+                        if (index != -1)  _cacheList[index] = newValue;
+                        break;
+                    case "Delete":
+                        newValue = logEntry.ModelJsonToObject<Cache>();
+                        originalValue = GetByKey(newValue.Key).Result;
+                        _cacheList.Remove(originalValue);
+                        break;
+                }
+            }
+        }
+
+        public  Task<Cache> GetByKey(string key)
+        {
+            // Get cache
+            return Task.FromResult(_cacheList.FirstOrDefault(CacheExpressions.HasKey(key).Compile()));
+        }
+        public void Add(Cache cache)
+        {
+            // Not supported
+            throw new NotSupportedException();
+        }
+    }
+}
