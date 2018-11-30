9b5b0dd8157dbf2f98d4155279eff03fbf809dfa
cesarbmx <cesarbmx@hotmail.com>
2018-11-30 17:51:35 -0400

    refactor

--- CryptoWatcher.Domain/Models/Entity.cs
+++ CryptoWatcher.Domain/Models/Entity.cs
@@ -4,9 +4,9 @@ namespace CryptoWatcher.Domain.Models
 {
     public abstract class Entity
     {
-        public string Id { get; protected set; }
+        public string Id { get; set; }
         //public User CreatedBy { get; set; }
-        public DateTime CreationTime { get; protected set; }
+        public DateTime CreationTime { get; private set; }
         //public User LastModifiedBy { get; set; }
         //public DateTime LastModificationTime { get; set; }
 
