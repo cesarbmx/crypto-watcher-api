3aab751587bf08291a2f14a01658d710429fb161
cesarbmx <cesarbmx@hotmail.com>
2018-12-18 12:07:22 -0400

    idem

--- CryptoWatcher.Domain/Expressions/LineExpression.cs
+++ CryptoWatcher.Domain/Expressions/LineExpression.cs
@@ -6,12 +6,6 @@ namespace CryptoWatcher.Domain.Expressions
 {
     public static class LineExpression
     {
-        public static Expression<Func<Line, bool>> Filter(string currencyId, string indicatorId)
-        {
-            return x =>
-                string.IsNullOrEmpty(currencyId) && x.CurrencyId == currencyId ||
-                x.IndicatorId == indicatorId;
-        }
         public static Expression<Func<Line, bool>> ObsoleteLine()
         {
             return x => x.CreationTime < DateTime.Now.AddDays(-7);
