8a9849ef2459c9cd2c711140c538ad2164c35e15
cesarbmx <cesarbmx@hotmail.com>
2018-12-26 17:21:05 -0400

    Add watcher validator

---
+++ CryptoWatcher.Application/Validators/AddWatcherValidator.cs
@@ -0,0 +1,16 @@
+﻿using CryptoWatcher.Application.Requests;
+using CryptoWatcher.Domain.Messages;
+using FluentValidation;
+
+namespace CryptoWatcher.Application.Validators
+{
+    public class AddWatcherValidator : AbstractValidator<AddWatcherRequest>
+    {
+        public AddWatcherValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty()
+                .WithMessage(nameof(UserMessage.UserIdCannotBeEmpty) + " " + UserMessage.UserIdCannotBeEmpty);
+        }
+    }
+}
