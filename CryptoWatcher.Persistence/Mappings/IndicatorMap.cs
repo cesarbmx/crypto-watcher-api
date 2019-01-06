40b8e79dcfdab7929633de4b76b96ab4b8b357b6
cesarbmx <cesarbmx@hotmail.com>
2019-01-05 22:50:41 -0400

    mapping

--- CryptoWatcher.Persistence/Mappings/IndicatorMap.cs
+++ CryptoWatcher.Persistence/Mappings/IndicatorMap.cs
@@ -45,6 +45,15 @@ namespace CryptoWatcher.Persistence.Mappings
                 .HasColumnType("nvarchar(max)")
                 .IsRequired();
 
+            entityBuilder.Property(t => t.Dependencies)
+                .HasColumnType("nvarchar(50)")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            entityBuilder.Property(t => t.DependencyLevel)
+                .HasColumnType("smallint")
+                .IsRequired();
+
             entityBuilder.Property(t => t.CreatedBy)
                 .HasColumnType("nvarchar(50)")
                 .HasMaxLength(50)
