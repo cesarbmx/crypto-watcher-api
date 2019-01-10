b3bf553009889844ac0cdb9636b79e04e9147b7b
cesarbmx <cesarbmx@hotmail.com>
2019-01-10 16:36:14 -0400

    data seed

--- CryptoWatcher.Persistence/Mappings/IndicatorMap.cs
+++ CryptoWatcher.Persistence/Mappings/IndicatorMap.cs
@@ -61,7 +61,7 @@ namespace CryptoWatcher.Persistence.Mappings
             entityBuilder.HasData(
                 new Indicator(IndicatorType.CurrencyIndicator, "price", "master", "Price", "", "", new List<IndicatorDependency>()),
                 new Indicator(IndicatorType.CurrencyIndicator, "price-change-24hrs", "master", "Price change 24Hrs", "", "", new List<IndicatorDependency>()),
-                new Indicator(IndicatorType.CurrencyIndicator, "hype", "master", "Hype", "", "", new List<IndicatorDependency>())
+                new Indicator(IndicatorType.CurrencyIndicator, "hype", "master", "Hype", "", "", new List<IndicatorDependency>{ new IndicatorDependency("hype", "price-change-24hrs") })
             );
         }
     }
