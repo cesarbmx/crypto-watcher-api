4bc2e43540b94835b7ea1e342780f2bf430097c1
cesarbmx <cesarbmx@hotmail.com>
2019-01-13 10:48:46 -0400

    remove unused code

--- CryptoWatcher.Domain/Expressions/IndicatorDependencyExpression.cs
+++ CryptoWatcher.Domain/Expressions/IndicatorDependencyExpression.cs
@@ -6,10 +6,6 @@ namespace CryptoWatcher.Domain.Expressions
 {
     public static class IndicatorDependencyExpression
     {
-        public static Expression<Func<IndicatorDependency, bool>> IndicatorDependency(string indicatorId, string dependsOn)
-        {
-            return x => x.IndicatorId == indicatorId && x.DependsOn == dependsOn;
-        }
         public static Expression<Func<IndicatorDependency, bool>> IndicatorDependencyFilter(string indicatorId, string dependsOn)
         {
             return x => (string.IsNullOrEmpty(indicatorId) || x.IndicatorId == indicatorId) &&
