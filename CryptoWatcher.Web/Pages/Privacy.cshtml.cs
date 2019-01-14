f0eabbc680be369a2f15b316177af84579cb9338
cesarbmx <cesarbmx@hotmail.com>
2019-01-14 09:25:16 -0400

    Initial Razor Pages project

---
+++ CryptoWatcher.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CryptoWatcher.Web.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
