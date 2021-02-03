39050823dfd8fb1bd053b8c213df51730d52ac83
cesarbmx <cesarbmx@hotmail.com>
2021-02-03 18:59:05 +0100

    update working

--- CryptoWatcher.Application/FakeRequests/UpdateIndicatorFakeRequest.cs
+++ CryptoWatcher.Application/FakeRequests/UpdateIndicatorFakeRequest.cs
@@ -14,7 +14,7 @@ namespace CryptoWatcher.Application.FakeRequests
                 Description = @"The Relative Strength Index (RSI) is a momentum oscillator that measures the speed and change of price movements.
                                 RSI oscillates between zero and 100. Traditionally, and according to Wilder, RSI is considered overbought when above 70 and oversold when below 30.",
                 Formula = "C# formula",
-                Dependencies = new[] {"price", "price-change-24hrs" }
+                Dependencies = new[] {"master.price", "master.price-change-24hrs" }
             };
         }       
     }
