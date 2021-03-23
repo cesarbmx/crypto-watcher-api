a27e8a0032ab5f3436245b0aaa4a4f6ee0f83b34
cesarbmx <cesarbmx@hotmail.com>
2021-03-23 14:45:27 +0100

    format

--- CesarBmx.CryptoWatcher.Application/Messages/WatcherMessage.cs
+++ CesarBmx.CryptoWatcher.Application/Messages/WatcherMessage.cs
@@ -6,8 +6,8 @@ namespace CesarBmx.CryptoWatcher.Application.Messages
     {
         public const string WatcherNotFound = "The watcher does not exist";
         public const string WatcherAlreadyExists = "The watcher already exists";
-        public const string WatcherAlreadyBought = "The watcher has bought already at {0}";
-        public const string WatcherAlreadySold = "The watcher has sold already at {0}";
+        public const string WatcherAlreadyBought = "The watcher has bought already at {0}$";
+        public const string WatcherAlreadySold = "The watcher has sold already at {0}$";
         public const string WatcherAlreadyLiquidated = "The watcher got liquidated already";
         public const string BuyLimitIsHigherThanWatcherValue = "The buy limit can't be higher than watcher's current value {0}$";
         public const string SellLimitIsLowerThanValue = "The sell limit can't be lower than watcher's current value {0}$";
