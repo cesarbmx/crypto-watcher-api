ef4b2bf9a3b0cac7a2bf7edfb62d477da93570f7
Cesar C <cesarbmx@hotmail.com>
2022-01-30 21:56:07 +0100

    ScriptVariablesValidator

---
+++ CesarBmx.CryptoWatcher.Application/QueryValidators/GetScriptVariablesValidator.cs
@@ -0,0 +1,16 @@
+﻿using CesarBmx.CryptoWatcher.Application.Queries;
+using CesarBmx.Shared.Application.Messages;
+using FluentValidation;
+
+namespace CesarBmx.CryptoWatcher.Application.QueryValidators
+{
+    public class GetScriptVariablesValidator : AbstractValidator<GetScriptVariables>
+    {
+        public GetScriptVariablesValidator()
+        {
+            RuleFor(x => x.Period)
+                .NotNull()
+                .WithMessage(nameof(ErrorMessage.Required) + " " + ErrorMessage.Required);
+        }
+    }
+}
