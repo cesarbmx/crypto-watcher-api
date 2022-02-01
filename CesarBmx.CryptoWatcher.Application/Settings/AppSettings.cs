3bd8f59fbd422be3ce447f5708afe8e458e63d1d
Cesar C <cesarbmx@hotmail.com>
2022-02-01 17:38:54 +0100

    Line retention in AppSettings

--- CesarBmx.CryptoWatcher.Application/Settings/AppSettings.cs
+++ CesarBmx.CryptoWatcher.Application/Settings/AppSettings.cs
@@ -1,6 +1,9 @@
 ﻿
 
 
+using System.Collections.Generic;
+using CesarBmx.CryptoWatcher.Domain.Types;
+
 namespace CesarBmx.CryptoWatcher.Application.Settings
 {
     public  class AppSettings: CesarBmx.Shared.Application.Settings.AppSettings
@@ -8,5 +11,6 @@ namespace CesarBmx.CryptoWatcher.Application.Settings
         public bool UseMemoryStorage { get; set; }
         public int JobsIntervalInMinutes { get; set; }
         public string TelegramApiToken { get; set; }
+        public Dictionary<LinePeriod, string> Values { get; set; }
     }
 }
